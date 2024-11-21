d57f77dfef5de5d44ada2fcb5e3c839c16b5e3ea
joaovictormat1ns <joaoaucindo@gmail.com>
2024-11-21 18:47:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'coisas do dia dia',
+    'oque a agua nos porpociona',
+    'ela nos idrata e faz bem para os rins'
+)
+
+criaCartao(
+    'equipamentos eletronicos',
+    'liquidificador',
+    'voce pode usalo para bater suas vitaminas'
+)
+
+criaCartao(
+    'curiosidades',
+    'sobre o seu cachorro',
+    'nao importa quantos ossos voce de a ele ele sempre ira enterralos'
+)
+
+criaCartao(
+    'fatos veridicos',
+    'voce sabia que',
+    'ao fazer exercícios voce pode aumentar o seu tempo de vida'
+)
